3e735e0ec8fd2617e4c7cf44793b3035fda2d6f4
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-11-16 10:50:36 -0500

    bio update

--- Components/About.js
+++ Components/About.js
@@ -10,7 +10,9 @@ export default function About() {
                 <h2 className='py-4'>Who I am </h2>
                 <p className='py-2 text-gray-600'>//</p>
                 <p className='py-2 text-gray-600'>I am junior full stack web developer looking for a full-time position. I have a background in business administration and  accounting,I bring strong problem-solving skills to the table. I am proactive and takes initiative to learn new things. In my free time, i enjoy watching soccer games and spending time with friends and family.</p>
-                <p className='py-2 text-gray-600'>...</p>
+                <p className='py-2 text-gray-600'>When the covid-19 lockdown began, I seized the opportunity to learn coding. 
+                I quickly realized that this was my passion, so I decided to enroll at Flatiron School to become a full stack web developer.
+                Now that I am a confident developer, I am eager to learn and grow with a team. My goal is to be the best developer that I can be and contribute value wherever possible.Please don't hesitate to contact me if you would like to discuss any opportunities.</p>
                 <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
 
             </div>
--- Components/ProjectItem.js
+++ Components/ProjectItem.js
@@ -6,7 +6,7 @@ import { project3 } from '../public/assets/project2 .jpg'
 
 export default function ProjectItem({title, background}) {
   return (
-    <div id='project' >
+    <div >
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#5651e5]'>
              <Image className='rounded-xl group-hover:opacity-10' src='/../public/assets/project1.jpg' width='1100' height='50' alt='/' />
                  <div className='hiddene group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%]'>
